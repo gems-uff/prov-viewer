ca3df53bb6bed573be1c1a44e3eb2afb419852e2
Troy Kohwalter <tkohwalter@gmail.com>
2015-06-22 18:25:13 -0300

    Updated GuiTooltip class to recursively compute the collapsed vertex tooltip when dealing with Graph vertices (vertices that represent previous collapses) inside the collapsed vertex Updated GuiTooltip to remove duplicate labels and timestamps when generating the collapsed vertex tooltip

--- src/main/java/br/uff/ic/provviewer/GUI/GuiTooltip.java
+++ src/main/java/br/uff/ic/provviewer/GUI/GuiTooltip.java
@@ -47,43 +47,21 @@ public class GuiTooltip {
         });
     }
 
+    static int agents = 0;
+    static int activities = 0;
+    static int entities = 0;
     public static String GraphTooltip(Object v) {
         String nodeTypes = "";
-        int agents = 0;
-        int activities = 0;
-        int entities = 0;
-        Map<String, GraphAttribute> attributes = new HashMap<String, GraphAttribute>();
-
-        String ids = "";
-        String labels = ""; //TODO HaseMap to avoid having repeated labels
-        String times = "";
-
-        Collection vertices = ((Graph) v).getVertices();
-        for (Object vertex : vertices) {
-            ids += ((Vertex) vertex).getID() + " ";
-            labels += ((Vertex) vertex).getLabel() + " ";
-            times += ((Vertex) vertex).getTime() + " ";
-            if (vertex instanceof AgentVertex) {
-                agents++;
-            } else if (vertex instanceof ActivityVertex) {
-                activities++;
-            } else if (vertex instanceof EntityVertex) {
-                entities++;
-            }
-//            else if (vertex instanceof Graph) 
-//            {
-//                // Do something
-//            }
-            for (Attribute att : ((Vertex) vertex).getAttributes()) {
-                if (attributes.containsKey(att.getName())) {
-                    GraphAttribute temporary = attributes.get(att.getName());
-                    temporary.updateAttribute(att.getValue());
-                    attributes.put(att.getName(), temporary);
-                } else {
-                    attributes.put(att.getName(), new GraphAttribute(att.getName(), att.getValue()));
-                }
-            }
-        }
+        
+        agents = 0;
+        activities = 0;
+        entities = 0;
+        Map<String, String> ids = new HashMap<String, String>();
+        Map<String, String> labels = new HashMap<String, String>();
+        Map<String, String> times = new HashMap<String, String>();
+        Map<String, GraphAttribute> attributes = new HashMap<String, GraphAttribute>(); 
+        
+        GraphTooltip(v, ids, labels, times, attributes);
 
         if (agents > 0) {
             nodeTypes = "Agents: " + agents + "<br>";
@@ -96,12 +74,50 @@ public class GuiTooltip {
         }
 
         return "<b>Collapsed Vertex" + "</b>" + "<br>" + nodeTypes
-                + "<br>IDs: " + ids + "<br>"
-                + "<b>Labels: " + labels + "</b>"
-                + " <br>" + "Times: " + times
+                + "<br>IDs: " + labels.values().toString() + "<br>"
+                + "<b>Labels: " + labels.values().toString() + "</b>"
+                + " <br>" + "Times: " + labels.values().toString()
                 + " <br>" + PrintAttributes(attributes);
     }
 
+    public static void GraphTooltip(Object v, 
+            Map<String, String> ids,
+            Map<String, String> labels,
+            Map<String, String> times,
+            Map<String, GraphAttribute> attributes){
+        
+        Collection vertices = ((Graph) v).getVertices();
+        for (Object vertex : vertices) {
+            if (!(vertex instanceof Graph))
+            {
+                ids.put(((Vertex) vertex).getID(), ((Vertex) vertex).getID());
+                labels.put(((Vertex) vertex).getLabel(), ((Vertex) vertex).getLabel());
+                times.put(((Vertex) vertex).getTimeString(), ((Vertex) vertex).getTimeString());
+                
+                if (vertex instanceof AgentVertex) {
+                    agents++;
+                } else if (vertex instanceof ActivityVertex) {
+                    activities++;
+                } else if (vertex instanceof EntityVertex) {
+                    entities++;
+                }
+                
+                for (Attribute att : ((Vertex) vertex).getAttributes()) {
+                    if (attributes.containsKey(att.getName())) {
+                        GraphAttribute temporary = attributes.get(att.getName());
+                        temporary.updateAttribute(att.getValue());
+                        attributes.put(att.getName(), temporary);
+                    } else {
+                        attributes.put(att.getName(), new GraphAttribute(att.getName(), att.getValue()));
+                    }
+                }
+            }
+            else //(vertex instanceof Graph) 
+            {
+                GraphTooltip(vertex, ids, labels, times, attributes);
+            }
+        }
+    }
     public static String PrintAttributes(Map<String, GraphAttribute> attributes) {
         String attributeList = "";
         if (!attributes.values().isEmpty()) {
