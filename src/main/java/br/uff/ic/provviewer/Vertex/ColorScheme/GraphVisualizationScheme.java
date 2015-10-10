e0f01d439a03dc9a1a8005b1b9e20767b264db6c
Troy Kohwalter <tkohwalter@gmail.com>
2015-10-10 19:42:45 -0300

    Created a new visualization scheme that paints vertices that belong to a particular ID group

---
+++ src/main/java/br/uff/ic/provviewer/Vertex/ColorScheme/GraphVisualizationScheme.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.uff.ic.provviewer.Vertex.ColorScheme;
+
+import br.uff.ic.provviewer.Variables;
+import br.uff.ic.utility.graph.ActivityVertex;
+import br.uff.ic.utility.graph.Vertex;
+import java.awt.Color;
+import java.awt.Paint;
+
+/**
+ *
+ * @author Kohwalter
+ */
+public class GraphVisualizationScheme extends ColorScheme {
+
+    public GraphVisualizationScheme(String attribute) {
+        super(attribute);
+    }
+
+    public GraphVisualizationScheme(String attribute, String empty, String g, String y, boolean l) {
+        super(attribute, empty, g, y, l);
+    }
+
+    @Override
+    public Paint Execute(Object v, final Variables variables) {
+        if ((v instanceof ActivityVertex) && (((Vertex)v).getID().contains(this.attribute)))
+            return new Color(255, 128, 0);
+        else
+            return ((Vertex) v).getColor();
+    }
+}
