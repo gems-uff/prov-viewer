d119085042b322476c61740e754a361b5322a715
Kohwalter <Kohwalter@DESKTOP-3EQCGG6>
2017-04-25 15:58:25 -0400

    Revert "Refactored test"
    This reverts commit 30b4ea8bc3a415aa4e0b6d23e754dfa29bbf93c6.

--- src/test/java/br/uff/ic/utility/graphgenerator/ClusteringEvaluatorTest.java
+++ src/test/java/br/uff/ic/utility/graphgenerator/ClusteringEvaluatorTest.java
@@ -46,24 +46,24 @@ public class ClusteringEvaluatorTest {
 //    int smallCluster = 7;
 //    int threshold = 4;
 //    int smallClusterMod = 4;
-    float dagEps;// = 288; // Dag previous 177.63
-    float linearEps;// = 380; // Linear 248
-    float treeEps;// = 364; // Tree 202.78
+    float dagEps = 288; // Dag previous 177.63
+    float linearEps = 380; // Linear 248
+    float treeEps = 364; // Tree 202.78
 //    float monotonicLinearEps = 312F;
 //    float monotonicDagEps = 268;
 //    float monotonicTreeEps = 328F;
-    float monotonicLinearEps;// = 40.64F; // Monotonic 312
-    float monotonicDagEps;// = 89.88F; // Monotonic 268
-    float monotonicTreeEps;// = 23.45F; // Monotonic 328
+    float monotonicLinearEps = 40.64F; // Monotonic 312
+    float monotonicDagEps = 89.88F; // Monotonic 268
+    float monotonicTreeEps = 23.45F; // Monotonic 328
     int TF_size;
     int TF_increase;
-    float TF_qnt;
+    int TF_qnt;
     int TT_size;
     int TT_increase;
-    float TT_qnt;
+    int TT_qnt;
     int FT_size;
     int FT_increase;
-    float FT_qnt;
+    int FT_qnt;
     
     public ClusteringEvaluatorTest() {
     }
@@ -89,16 +89,6 @@ public class ClusteringEvaluatorTest {
      */
     @Test
     public void testCollapse() throws Exception {
-        TF_size = 5;
-        TT_size = 5;
-        FT_size = 5;
-        
-        linearEps = 1.0f;//142;
-        dagEps = 1.1f;//190;
-        treeEps = 1.3f;//190;
-        monotonicLinearEps = 0.2f;//26;
-        monotonicDagEps = 0.2f;//22;
-        monotonicTreeEps = 0.2f;//18;
 //        train();
 //        runExperiment();
 //        trainForExperiment();
@@ -110,7 +100,7 @@ public class ClusteringEvaluatorTest {
     }
 
     private void train() throws IOException, InterruptedException {
-//        trainDBSCAN();
+        trainDBSCAN();
         trainSimilarity();
     }
 
@@ -118,54 +108,50 @@ public class ClusteringEvaluatorTest {
         boolean isMonotonic = false;
         
         // Train
-//        Trainer trainer = new Trainer();
-//        trainer.setMonotonic(isMonotonic);
-        String graphType = "Dag";
-//        int iterations = 1;
-//        int numberOracles = 10;
-//        int numberNoiseGraphs = 5;
+        Trainer trainer = new Trainer();
+        trainer.setMonotonic(isMonotonic);
+        String graphType = "Tree";
+        int iterations = 1;
+        int numberOracles = 10;
+        int numberNoiseGraphs = 5;
         int initialNoise = 10;
-//        int noiseIncrease = 1;
-//        System.out.println("DBSCAN");
-//        float epsilon = trainer.trainDBSCAN(oracle, iterations, numberOracles, numberNoiseGraphs,  initialNoise, noiseIncrease, graphType);
-//        System.out.println("True True");
-//        trainer.trainSimilarity(true, true, oracle, iterations, numberOracles, numberOracles, numberNoiseGraphs, noiseIncrease, graphType);
-//        System.out.println("True False");
-//        trainer.trainSimilarity(true, false, oracle, iterations, numberOracles, numberNoiseGraphs, initialNoise, noiseIncrease, graphType);
-//        System.out.println("False True");
-//        trainer.trainSimilarity(false, true, oracle, iterations, numberOracles, numberNoiseGraphs, initialNoise, noiseIncrease, graphType);
+        int noiseIncrease = 1;
+        System.out.println("DBSCAN");
+        float epsilon = trainer.trainDBSCAN(oracle, iterations, numberOracles, numberNoiseGraphs,  initialNoise, noiseIncrease, graphType);
+        System.out.println("True True");
+        trainer.trainSimilarity(true, true, oracle, iterations, numberOracles, numberOracles, numberNoiseGraphs, noiseIncrease, graphType);
+        System.out.println("True False");
+        trainer.trainSimilarity(true, false, oracle, iterations, numberOracles, numberNoiseGraphs, initialNoise, noiseIncrease, graphType);
+        System.out.println("False True");
+        trainer.trainSimilarity(false, true, oracle, iterations, numberOracles, numberNoiseGraphs, initialNoise, noiseIncrease, graphType);
         
         // Generate the experiment graph
         DirectedGraph<Object, Edge> oracleGraph = eval.oracleGraph.createOracleGraph(graphType);
         NoiseGraph instance = new NoiseGraph(oracleGraph, "A", isMonotonic);
         DirectedGraph<Object, Edge> noiseGraph = instance.generateNoiseGraph(initialNoise, 1.0F, "");
         Utils.exportGraph(noiseGraph, "Experiment_graph_" + graphType + "_" + "01");
-        instance = new NoiseGraph(oracleGraph, "A", isMonotonic);
         noiseGraph = instance.generateNoiseGraph(initialNoise, 1.0F, "");
         Utils.exportGraph(noiseGraph, "Experiment_graph_" + graphType + "_" + "02");
-        instance = new NoiseGraph(oracleGraph, "A", isMonotonic);
         noiseGraph = instance.generateNoiseGraph(initialNoise, 1.0F, "");
         Utils.exportGraph(noiseGraph, "Experiment_graph_" + graphType + "_" + "03");
-        instance = new NoiseGraph(oracleGraph, "A", isMonotonic);
         noiseGraph = instance.generateNoiseGraph(initialNoise, 1.0F, "");
         Utils.exportGraph(noiseGraph, "Experiment_graph_" + graphType + "_" + "04");
-        instance = new NoiseGraph(oracleGraph, "A", isMonotonic);
         noiseGraph = instance.generateNoiseGraph(initialNoise, 1.0F, "");
         Utils.exportGraph(noiseGraph, "Experiment_graph_" + graphType + "_" + "05");
     }
     
     
     private void trainDBSCAN() throws IOException {
-        trainDBSCANRandom();
         trainDBSCANMonotonic();
+        trainDBSCANRandom();
     }
 
     private void trainDBSCANMonotonic() throws IOException {
         Trainer trainer = new Trainer();
         trainer.setMonotonic(true);
-        monotonicLinearEps = trainer.trainDBSCAN(oracle, "Linear");
-        monotonicDagEps = trainer.trainDBSCAN(oracle, "DAG");
-        monotonicTreeEps = trainer.trainDBSCAN(oracle, "TREE");
+        monotonicLinearEps = trainer.trainDBSCAN(oracle, NUMBER_ITERATIONS, NUMBER_OF_ORACLE_GRAPHS, NUMBER_OF_NOISE_GRAPHS, INITIAL_NOISE_GRAPH_SIZE, NOISE_INCREASE_NUMBER, "Linear");
+        monotonicDagEps = trainer.trainDBSCAN(oracle, NUMBER_ITERATIONS, NUMBER_OF_ORACLE_GRAPHS, NUMBER_OF_NOISE_GRAPHS, INITIAL_NOISE_GRAPH_SIZE, NOISE_INCREASE_NUMBER, "DAG");
+        monotonicTreeEps = trainer.trainDBSCAN(oracle, NUMBER_ITERATIONS,  NUMBER_OF_ORACLE_GRAPHS, NUMBER_OF_NOISE_GRAPHS, INITIAL_NOISE_GRAPH_SIZE, NOISE_INCREASE_NUMBER, "TREE");
         System.out.println("Best monotonicLinear: " + monotonicLinearEps);
         System.out.println("Best monotonicDAG: " + monotonicDagEps);
         System.out.println("Best monotonicTREE: " + monotonicTreeEps);
@@ -174,9 +160,9 @@ public class ClusteringEvaluatorTest {
     private void trainDBSCANRandom() throws IOException {
         Trainer trainer = new Trainer();
         trainer.setMonotonic(false);
-        linearEps = trainer.trainDBSCAN(oracle, "Linear");
-        dagEps = trainer.trainDBSCAN(oracle, "DAG");
-        treeEps = trainer.trainDBSCAN(oracle, "TREE");
+        linearEps = trainer.trainDBSCAN(oracle, NUMBER_ITERATIONS, NUMBER_OF_ORACLE_GRAPHS, NUMBER_OF_NOISE_GRAPHS, INITIAL_NOISE_GRAPH_SIZE, NOISE_INCREASE_NUMBER, "Linear");
+        dagEps = trainer.trainDBSCAN(oracle, NUMBER_ITERATIONS, NUMBER_OF_ORACLE_GRAPHS, NUMBER_OF_NOISE_GRAPHS, INITIAL_NOISE_GRAPH_SIZE, NOISE_INCREASE_NUMBER, "DAG");
+        treeEps = trainer.trainDBSCAN(oracle, NUMBER_ITERATIONS, NUMBER_OF_ORACLE_GRAPHS, NUMBER_OF_NOISE_GRAPHS, INITIAL_NOISE_GRAPH_SIZE, NOISE_INCREASE_NUMBER, "TREE");
 
         System.out.println("Best Linear: " + linearEps);
         System.out.println("Best DAG: " + dagEps);
@@ -195,98 +181,104 @@ public class ClusteringEvaluatorTest {
         trainer.setMonotonic(true);
         System.out.println("True FALSE Monotonic");
         System.out.println("Linear");
-        linearEps = trainer.trainSimilarity(true, false, oracle, "Linear", monotonicLinearEps);
+        linearEps = trainer.trainSimilarity(true, false, oracle, 5, 5, 3, 10, 2, "Linear");
         System.out.println("Dag");
-        dagEps = trainer.trainSimilarity(true, false, oracle, "DAG", monotonicDagEps);
+        dagEps = trainer.trainSimilarity(true, false, oracle, 5, 5, 3, 10, 2, "DAG");
         System.out.println("Tree");
-        treeEps = trainer.trainSimilarity(true, false, oracle, "TREE", monotonicTreeEps);
+        treeEps = trainer.trainSimilarity(true, false, oracle, 5, 5, 3, 10, 2, "TREE");
 
         trainer = new Trainer();
         trainer.setMonotonic(true);
         System.out.println("True True Monotonic");
         System.out.println("Linear");
-        trainer.trainSimilarity(true, true, oracle, "Linear", monotonicLinearEps);
+        trainer.trainSimilarity(true, true, oracle, 5, 5, 3, 10, 2, "Linear");
         System.out.println("Dag");
-        trainer.trainSimilarity(true, true, oracle, "DAG", monotonicDagEps);
+        trainer.trainSimilarity(true, true, oracle, 5, 5, 3, 10, 2, "DAG");
         System.out.println("Tree");
-        trainer.trainSimilarity(true, true, oracle, "TREE", monotonicTreeEps);
+        trainer.trainSimilarity(true, true, oracle, 5, 5, 3, 10, 2, "TREE");
 
         trainer = new Trainer();
         trainer.setMonotonic(true);
         System.out.println("False True Monotonic");
         System.out.println("Linear");
-        trainer.trainSimilarity(false, true, oracle, "Linear", monotonicLinearEps);
+        trainer.trainSimilarity(false, true, oracle, 5, 5, 3, 10, 2, "Linear");
         System.out.println("Dag");
-        trainer.trainSimilarity(false, true, oracle, "DAG", monotonicDagEps);
+        trainer.trainSimilarity(false, true, oracle, 5, 5, 3, 10, 2, "DAG");
         System.out.println("Tree");
-        trainer.trainSimilarity(false, true, oracle, "TREE", monotonicTreeEps);
+        trainer.trainSimilarity(false, true, oracle, 5, 5, 3, 10, 2, "TREE");
     }
 
     private void trainSimilarityRandom() throws IOException, InterruptedException {
         Trainer trainer = new Trainer();
         System.out.println("======Similarity Training RANDOM===========");
-//        trainer = new Trainer();
-//        trainer.setMonotonic(false);
-//        System.out.println("True FALSE Normal");
-//        System.out.println("Linear");
-//        linearEps = trainer.trainSimilarity(true, false, oracle, "Linear", linearEps);
-//        System.out.println("Dag");
-//        dagEps = trainer.trainSimilarity(true, false, oracle, "DAG", dagEps);
-//        System.out.println("Tree");
-//        treeEps = trainer.trainSimilarity(true, false, oracle, "TREE", treeEps);
+        trainer = new Trainer();
+        trainer.setMonotonic(false);
+        System.out.println("True FALSE Normal");
+        System.out.println("Linear");
+        linearEps = trainer.trainSimilarity(true, false, oracle, 5, 5, 3, 10, 2, "Linear");
+        System.out.println("Dag");
+        dagEps = trainer.trainSimilarity(true, false, oracle, 5, 5, 3, 10, 2, "DAG");
+        System.out.println("Tree");
+        treeEps = trainer.trainSimilarity(true, false, oracle, 5, 5, 3, 10, 2, "TREE");
         
         trainer = new Trainer();
         trainer.setMonotonic(false);
         System.out.println("True True Normal");
-//        System.out.println("Linear");
-//        trainer.trainSimilarity(true, true, oracle, "Linear", linearEps);
-//        System.out.println("Dag");
-//        trainer.trainSimilarity(true, true, oracle, "DAG", dagEps);
+        System.out.println("Linear");
+        trainer.trainSimilarity(true, true, oracle, 5, 5, 3, 10, 2, "Linear");
+        System.out.println("Dag");
+        trainer.trainSimilarity(true, true, oracle, 5, 5, 3, 10, 2, "DAG");
         System.out.println("Tree");
-        trainer.trainSimilarity(true, true, oracle, "TREE", treeEps);
+        trainer.trainSimilarity(true, true, oracle, 5, 5, 3, 10, 2, "TREE");
         
         trainer = new Trainer();
         trainer.setMonotonic(false);
         System.out.println("Linear");
         System.out.println("False True Normal");
-        trainer.trainSimilarity(false, true, oracle, "Linear", linearEps);
+        trainer.trainSimilarity(false, true, oracle, 5, 5, 3, 10, 2, "Linear");
         System.out.println("Dag");
-        trainer.trainSimilarity(false, true, oracle, "DAG", dagEps);
+        trainer.trainSimilarity(false, true, oracle, 5, 5, 3, 10, 2, "DAG");
         System.out.println("Tree");
-        trainer.trainSimilarity(false, true, oracle, "TREE", treeEps);
+        trainer.trainSimilarity(false, true, oracle, 5, 5, 3, 10, 2, "TREE");
     }
 
     private void normalGraph() throws InterruptedException {
          try {
              System.out.println("Normal Graphs");
              eval = new ClusteringEvaluator(false, oracle);
-             
-             TF_increase = 7;
-             TF_qnt = 3.7f;
-             TT_increase = 6;
-             TT_qnt = 5.3f;
-             FT_increase = 1;
-             FT_qnt = 2.3f;
+             TF_size = 5;
+             TF_increase = 13;
+             TF_qnt = 6;
+             TT_size = 5;
+             TT_increase = 12;
+             TT_qnt = 5;
+             FT_size = 5;
+             FT_increase = 13;
+             FT_qnt = 4;
              eval.collapse(NUMBER_OF_ORACLE_GRAPHS, NUMBER_OF_NOISE_GRAPHS, INITIAL_NOISE_GRAPH_SIZE, NOISE_INCREASE_NUMBER, NUMBER_ITERATIONS, "Linear", "Linear", linearEps, TF_size, TF_increase, TF_qnt, TT_size, TT_increase, TT_qnt, FT_size, FT_increase, FT_qnt);
              
              eval = new ClusteringEvaluator(false, oracle);
-
-             TF_increase = 7;
-             TF_qnt = 3.7f;
-             TT_increase = 2;
-             TT_qnt = 5.1f;
-             FT_increase = 1;
-             FT_qnt = 2.5f;
+             TF_size = 5;
+             TF_increase = 13;
+             TF_qnt = 12;
+             TT_size = 5;
+             TT_increase = 6;
+             TT_qnt = 12;
+             FT_size = 7;
+             FT_increase = 6;
+             FT_qnt = 12;
              eval.collapse(NUMBER_OF_ORACLE_GRAPHS, NUMBER_OF_NOISE_GRAPHS, INITIAL_NOISE_GRAPH_SIZE, NOISE_INCREASE_NUMBER, NUMBER_ITERATIONS, "DAG", "DAG", dagEps, TF_size, TF_increase, TF_qnt, TT_size, TT_increase, TT_qnt, FT_size, FT_increase, FT_qnt);
              
              eval = new ClusteringEvaluator(false, oracle);
-
-             TF_increase = 6;
-             TF_qnt = 3.5f;
-             TT_increase = 5;
-             TT_qnt = 5.1f;
-             FT_increase = 1;
-             FT_qnt = 2.5f;
+             TF_size = 5;
+             TF_increase = 9;
+             TF_qnt = 6;
+             TT_size = 5;
+             TT_increase = 9;
+             TT_qnt = 5;
+             FT_size = 5;
+             FT_increase = 10;
+             FT_qnt = 5;
              eval.collapse(NUMBER_OF_ORACLE_GRAPHS, NUMBER_OF_NOISE_GRAPHS, INITIAL_NOISE_GRAPH_SIZE, NOISE_INCREASE_NUMBER, NUMBER_ITERATIONS, "TREE", "TREE", treeEps, TF_size, TF_increase, TF_qnt, TT_size, TT_increase, TT_qnt, FT_size, FT_increase, FT_qnt);
          } catch (IOException ex) {
              Logger.getLogger(ClusteringEvaluatorTest.class.getName()).log(Level.SEVERE, null, ex);
@@ -298,33 +290,39 @@ public class ClusteringEvaluatorTest {
              System.out.println("Partial-Monotonic Graphs");
              // Monotonic
              eval = new ClusteringEvaluator(true, oracle);
-
-             TF_increase = 7;
-             TF_qnt = 4.9f;
-             TT_increase = 7;
-             TT_qnt = 6.7f;
-             FT_increase = 1;
-             FT_qnt = 0.5f;
+             TF_size = 5;
+             TF_increase = 11;
+             TF_qnt = 12;
+             TT_size = 5;
+             TT_increase = 4;
+             TT_qnt = 12;
+             FT_size = 5; //To TRAIN
+             FT_increase = 12;  //To TRAIN
+             FT_qnt = 12; //To TRAIN
              eval.collapse(NUMBER_OF_ORACLE_GRAPHS, NUMBER_OF_NOISE_GRAPHS, INITIAL_NOISE_GRAPH_SIZE, NOISE_INCREASE_NUMBER, NUMBER_ITERATIONS, "Monotonic_Linear", "Linear", monotonicLinearEps, TF_size, TF_increase, TF_qnt, TT_size, TT_increase, TT_qnt, FT_size, FT_increase, FT_qnt);
 //             
              eval = new ClusteringEvaluator(true, oracle);
-
-             TF_increase = 5;
-             TF_qnt = 6.3f;
-             TT_increase = 6;
-             TT_qnt = 6.5f;
-             FT_increase = 1;
-             FT_qnt = 0.5f;
+             TF_size = 5;
+             TF_increase = 8;
+             TF_qnt = 12;
+             TT_size = 5;
+             TT_increase = 10;
+             TT_qnt = 12;
+             FT_size = 5;  //To TRAIN
+             FT_increase = 6;  //To TRAIN
+             FT_qnt = 12; //To TRAIN
              eval.collapse(NUMBER_OF_ORACLE_GRAPHS, NUMBER_OF_NOISE_GRAPHS, INITIAL_NOISE_GRAPH_SIZE, NOISE_INCREASE_NUMBER, NUMBER_ITERATIONS, "Monotonic_Dag", "DAG", monotonicDagEps, TF_size, TF_increase, TF_qnt, TT_size, TT_increase, TT_qnt, FT_size, FT_increase, FT_qnt);
              
              eval = new ClusteringEvaluator(true, oracle);
-
-             TF_increase = 5;
-             TF_qnt = 5.7f;
-             TT_increase = 6;
-             TT_qnt = 6.5f;
-             FT_increase = 1;
-             FT_qnt = 0.5f; // Training yet
+             TF_size = 5;
+             TF_increase = 13;
+             TF_qnt = 12;
+             TT_size = 5;
+             TT_increase = 12;
+             TT_qnt = 12;
+             FT_size = 5; //To TRAIN
+             FT_increase = 13;  //To TRAIN
+             FT_qnt = 12; //To TRAIN
              eval.collapse(NUMBER_OF_ORACLE_GRAPHS, NUMBER_OF_NOISE_GRAPHS, INITIAL_NOISE_GRAPH_SIZE, NOISE_INCREASE_NUMBER, NUMBER_ITERATIONS, "Monotonic_TREE", "TREE", monotonicTreeEps, TF_size, TF_increase, TF_qnt, TT_size, TT_increase, TT_qnt, FT_size, FT_increase, FT_qnt);
          } catch (IOException ex) {
              Logger.getLogger(ClusteringEvaluatorTest.class.getName()).log(Level.SEVERE, null, ex);
