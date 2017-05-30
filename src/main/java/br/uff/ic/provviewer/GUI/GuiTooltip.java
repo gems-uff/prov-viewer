da60d575d60f9642c78a005b964780f54df1b4bd
Kohwalter <tkohwalter@gmail.com>
2017-05-30 17:59:34 -0400

    Refactored to work with GraphVertex and removing the old code for "instanceof Graph"

--- src/main/java/br/uff/ic/provviewer/GUI/GuiTooltip.java
+++ src/main/java/br/uff/ic/provviewer/GUI/GuiTooltip.java
@@ -26,16 +26,8 @@ package br.uff.ic.provviewer.GUI;
 import br.uff.ic.utility.graph.Edge;
 import br.uff.ic.utility.GraphAttribute;
 import br.uff.ic.provviewer.Variables;
-import br.uff.ic.utility.GraphUtils;
-import br.uff.ic.utility.graph.ActivityVertex;
-import br.uff.ic.utility.graph.AgentVertex;
-import br.uff.ic.utility.graph.EntityVertex;
-import br.uff.ic.utility.graph.GraphVertex;
 import br.uff.ic.utility.graph.Vertex;
-import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.collections15.Transformer;
 
@@ -44,9 +36,6 @@ import org.apache.commons.collections15.Transformer;
  * @author Kohwalter
  */
 public class GuiTooltip {
-//    static int agents = 0;
-//    static int activities = 0;
-//    static int entities = 0;
     Map<String, GraphAttribute> attributes;
 
     /**
@@ -59,9 +48,6 @@ public class GuiTooltip {
         variables.view.setVertexToolTipTransformer(new ToStringLabeller() {
             @Override
             public String transform(Object v) {
-                if (v instanceof Graph) {
-                    return "<html>" + GraphTooltip(v) + "</html>";
-                }
                 ((Vertex) v).setTimeScalePrint(variables.config.timeScale, variables.selectedTimeScale);
                 return "<html>" + v.toString() + "</html>";
             }
@@ -74,70 +60,5 @@ public class GuiTooltip {
                 return "<html><font size=\"4\">" + n.getEdgeTooltip() + "</html>";
             }
         });
-    }
-
-    /**
-     * Tooltip generator for collapsed vertices, also known as Graph vertices or Composite vertices
-     * @param v represents the targeted vertex for the tooltip 
-     * @return a string that is the tooltip
-     */
-    public static String GraphTooltip(Object v) {
-        return GraphUtils.CreateVertexGraph(v).toString();
-//        return PrintVertexGraph(v, ids, labels, times, attributes);
-    }
-    
-    /**
-     * Method to print the Graph-Vertex
-     * This is no longer used
-     * @deprecated 
-     * @param v
-     * @param ids
-     * @param labels
-     * @param times
-     * @param attributes
-     * @return 
-     */
-    public static String PrintVertexGraph(Object v, 
-            Map<String, String> ids,
-            Map<String, String> labels,
-            Map<String, String> times,
-            Map<String, GraphAttribute> attributes){
-        String nodeTypes = "";
-        int agents = 0;
-        int activities = 0;
-        int entities = 0;
-        
-        if (agents > 0) {
-            nodeTypes = "Agents: " + agents + "<br>";
-        }
-        if (activities > 0) {
-            nodeTypes += "Activities: " + activities + "<br>";
-        }
-        if (entities > 0) {
-            nodeTypes += "Entities: " + entities + "<br>";
-        }
-
-        return "<b>Summarized Vertex" + "</b>" + "<br>" + nodeTypes
-                + "<br>IDs: " + ids.values().toString() + "<br>"
-                + "<b>Labels: " + labels.values().toString() + "</b>"
-                + " <br>" + "Original Timestamps: " + times.values().toString()
-                + " <br>" + PrintAttributes(attributes);
-    }
-
-    
-    
-    /**
-     * Prints the tooltip attributes
-     * @param attributes is the list of all attributes
-     * @return a string that represents all attributes and their respective values
-     */
-    public static String PrintAttributes(Map<String, GraphAttribute> attributes) {
-        String attributeList = "";
-        if (!attributes.values().isEmpty()) {
-            for (GraphAttribute att : attributes.values()) {
-                attributeList += att.printAttribute();
-            }
-        }
-        return attributeList;
     }
 }
